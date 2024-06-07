b9f403b0f7c0775b9c6418278bd595339ad23133
Tirtza Licht <37325924926@mby.co.il>
2024-06-07 09:07:37 +0300

    The calc

---
+++ 2023_11_19_Calc/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Func<double, double, double> OurDelegate;
+
+Console.WriteLine("Enter a Exc example: 5 - 6");
+string ex = Console.ReadLine();
+string[] exr=ex.Split(' ');
+if (exr.Length != 3)
+{
+    Console.WriteLine($"Oopppps :( , Did you see the example???");
+    return;
+}
+
+switch (exr[1])
+{
+    case "+":
+        OurDelegate = (x, y) => x + y;
+        break;
+    case "-":
+        OurDelegate = (x, y) => x - y;
+        break;
+    case "*":
+        OurDelegate = (x, y) => x * y;
+        break;
+    case "/":
+        OurDelegate = (x, y) => x / y;
+        break;
+    default:
+        OurDelegate = (x, y) => double.NaN;
+        break;
+}
+Console.WriteLine(OurDelegate(Convert.ToDouble(exr[0]), Convert.ToDouble(exr[2])));
